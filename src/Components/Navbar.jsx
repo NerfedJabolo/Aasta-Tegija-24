a0d4ae9b4142642a546e6958ebf8a47d2fd05d92
NerfedJabolo <ezdlprofit1@gmail.com>
2024-01-17 20:28:03 +0200

    Implement SPA Logic and add About Us page ( 2pt / 5pt)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,9 @@ const Navbar = () => {
   return (
     <nav className="w-[800px] max-w-[100vw] h-16 bg-[rgb(202,_219,_54)] right-0 top-0 absolute">
       <ul className="text-[20px] w-full flex justify-evenly items-center h-full">
-        <li className="text-center">About us</li>
+        <li className="text-center">
+          <a href="/about">About us</a>
+        </li>
         <li className="text-center">Timetable</li>
         <li className="text-center">Contacts</li>
         <li className="text-center">Additional Info</li>
