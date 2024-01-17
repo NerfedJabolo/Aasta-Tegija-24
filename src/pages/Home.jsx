a1361a217256abdc4cef708b261830545f26c72f
NerfedJabolo <ezdlprofit1@gmail.com>
2024-01-17 19:27:53 +0200

    Create navbar and add it to the Hero Section ( 3pt / 5pt)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import Navbar from '../Components/Navbar';
+
 const bannerGif = '/images/optimized-banner.gif';
 const logoText = '/images/logo2024 1.png';
 const imageAndPattern = '../../public/images/pilt ja muster.png';
@@ -9,6 +11,7 @@ const Home = () => {
     <>
       <img src={bannerGif} alt="" className="w-full" />
       <div className="h-[1000px] w-full bg-black relative flex items-end">
+        <Navbar />
         <div className="w-[500px] h-[500px] bottom-0 right-0 absolute triangle-container flex justify-between items-center">
           <h1 className="text-[40px] text-center text-white z-10 self-start justify-self-start -ml-36 -mt-12">
             XXIX Baltic Games for Vocational Schools
