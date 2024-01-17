4a9fcded7e5a9644ef50f1204cf4b6eb8b5212e6
NerfedJabolo <ezdlprofit1@gmail.com>
2024-01-17 15:03:11 +0200

    Add Hero Section for Homepage (4pt / 5pt)

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,32 @@
 const bannerGif = '/images/optimized-banner.gif';
+const logoText = '/images/logo2024 1.png';
+const imageAndPattern = '../../public/images/pilt ja muster.png';
+const ekssl = '../../public/images/ekksl logo.png';
+const vocoLogo = '../../public/images/voco logo.png';
 
 const Home = () => {
   return (
     <>
       <img src={bannerGif} alt="" className="w-full" />
+      <div className="h-[1000px] w-full bg-black relative flex items-end">
+        <div className="w-[500px] h-[500px] bottom-0 right-0 absolute triangle-container flex justify-between items-center">
+          <h1 className="text-[40px] text-center text-white z-10 self-start justify-self-start -ml-36 -mt-12">
+            XXIX Baltic Games for Vocational Schools
+          </h1>
+          <img src={logoText} alt="" className="mr-6 z-10" />
+        </div>
+        <div>
+          <img
+            src={imageAndPattern}
+            alt=""
+            className="md:w-[700px] w-[600px]"
+          />
+          <span className="flex mb-6">
+            <img src={ekssl} alt="" />
+            <img src={vocoLogo} alt="" />
+          </span>
+        </div>
+      </div>
     </>
   );
 };
